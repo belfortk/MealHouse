56a287132ed09382af8028100ff21c914cbee7f9
Daniel Kurtz <danielmkurtz@gmail.com>
2017-11-13 15:54:33 -0800

    Added proper seed data

--- src/app.jsx
+++ src/app.jsx
@@ -5,14 +5,13 @@ import SearchBar from "./SearchBar";
 import React, { Component } from 'react';
 import MenuProfile from './MenuProfile';
 
-
 import SignupPage from './SignupPage/SignupPage'
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+   
   }
 
   }
@@ -20,16 +19,14 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="App" >
         <header className="App-header">
           <Navbar />
           <LoginModal/>
         </header>
 
-        <SearchBar />
+      <SearchBar />
         <div className="container">
-
-
         {/* <SignupPage /> */}
 
         </div>
