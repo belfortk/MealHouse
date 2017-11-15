5f3ee2c9c32008e98d4e6c98b1197691ea8f36eb
Christopher Stevens <crsstevens1@gmail.com>
2017-11-15 13:57:19 -0800

    added redux to searchbar

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import customerSign from './SignupPage/CustomerReducer';
-import restaurantSign from './SignupPage/RestaurantReducer'
+import restaurantSign from './SignupPage/RestaurantReducer';
+import search from './SearchReducer';
 
 
 const rootReducer = combineReducers({
     customerSign,
-    restaurantSign
+    restaurantSign,
+    search
 });
 
 export default rootReducer;
